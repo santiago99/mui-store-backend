f265e92a57581337e9d9445769eb87fcacb9a789
Pavel Zobnin <mbair@192.168.1.55>
2025-10-19 16:25:45 -0300

    FIX!: auth routes now have /api/v1 prefix; unused route removed

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,18 @@ use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProductController;
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::get('/user', fn (Request $request) => $request->user());
+    Route::get('/user', fn(Request $request) => $request->user());
     Route::apiResource('posts', PostController::class);
 
     // Cart routes
@@ -27,7 +34,7 @@ Route::prefix('v1')->group(function () {
     Route::apiResource('categories', CategoryController::class);
 
     // Additional category routes
-    Route::get('categories-tree', [CategoryController::class, 'tree']);
+    //Route::get('categories-tree', [CategoryController::class, 'tree']);
     Route::post('categories/{category}/move', [CategoryController::class, 'move']);
     Route::get('categories/{category}/products', [CategoryController::class, 'products']);
 });
