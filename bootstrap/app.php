bc0f712a291cbad9d622a3add4045edf7876af8e
Pavel Zobnin <paul.zobnin@gmail.com>
2025-10-24 23:39:10 -0300

    [FEAT] Log current route params to debug log (optional, .env)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Middleware\LogRouteActivity;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\LogRouteActivity;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -21,9 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
-        if (env('LOG_ROUTE_ACTIVITY', false)) {
-            $middleware->append(LogRouteActivity::class);
-        }
+        $middleware->append(LogRouteActivity::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
